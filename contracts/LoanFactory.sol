200b6e1a9b05ee70b8675761d7ddf7594333c2f7
Matthew Black <mblackmblackmblack@gmail.com>
2018-06-09 14:57:31 -0400

    Fix Loan and LoanFactory for additional dot address argument

--- contracts/LoanFactory.sol
+++ contracts/LoanFactory.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.4.21;
 import './Loan.sol';
 
 contract LoanFactory {
-	function createLoan(uint[] _periodArray, address[] _contractAddressesArray) public returns(address) {
-		Loan loan = new Loan(_periodArray, _contractAddressesArray);
+	function createLoan(uint[] _periodArray, address[] _contractAddressesArray, address dotAddress) public returns(address) {
+		Loan loan = new Loan(_periodArray, _contractAddressesArray, dotAddress);
 		return address(loan);
 	}
 }
