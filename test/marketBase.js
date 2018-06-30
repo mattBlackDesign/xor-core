c6e3f0766721a41109f1a0d4a03435eec1b3a827
Matthew Black <mblackmblackmblack@gmail.com>
2018-06-30 17:57:23 -0400

    Update deploy

--- test/marketBase.js
+++ test/marketBase.js
@@ -1,17 +1,28 @@
 var MarketBase = artifacts.require("./MarketBase.sol");
+var ExampleLoanTrust = artifacts.require("xor-external-contract-examples/contracts/ExampleLoanTrust.sol");
+var ExampleLoanInterest = artifacts.require("xor-external-contract-examples/contracts/ExampleLoanInterest.sol");
+var Token = artifacts.require("./Token.sol");
+var Loan = artifacts.require("./Loan.sol");
 
 contract('MarketBase', function(accounts) {
 
-  // it("...should store the value 89.", function() {
-  //   return SimpleStorage.deployed().then(function(instance) {
-  //     simpleStorageInstance = instance;
+  beforeEach(async function () {
+    this.marketBase = await MarketBase.deployed();
+    this.loanTrust = await ExampleLoanTrust.deployed();
+    this.loanInterest = await ExampleLoanInterest.deployed();
+    this.token = await Token.deployed();
+    this.createMarket = await this.marketBase.createMarket(
+      [60 * 2, 60 * 2, 60 * 2],
+      [this.loanTrust.address, this.loanInterest.address, this.token.address]
+    );
+    this.loanAddress = this.createMarket.logs[0].args.contractAddress;
+    this.loan = Loan.at(this.loanAddress);
+  });
 
-  //     return simpleStorageInstance.set(89, {from: accounts[0]});
-  //   }).then(function() {
-  //     return simpleStorageInstance.get.call();
-  //   }).then(function(storedData) {
-  //     assert.equal(storedData, 89, "The value 89 was not stored.");
-  //   });
-  // });
-
+  describe('test', function() {
+    it('should return', async function() {
+      const isBorrower = await this.loan.borrower("0x0000000000000000000000000000000000000000");
+      assert.equal(isBorrower,false);
+    })
+  })
 });
